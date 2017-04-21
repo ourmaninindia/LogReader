d153f50558bddaeb362acf8904bbe3236a376edf
Alfred Tuinman <alfred@ourmaninindia.com>
2017-04-21 19:04:44 +0200

    date issues

--- lib/LogReader/Database.pm
+++ lib/LogReader/Database.pm
@@ -608,10 +608,16 @@ sub numrows_errorlogs
         $option .= " and status like 'crit' "; 
     }
  
-    my  $qry  =  "SELECT  count(*) as numrows, firstdate, lastdate FROM (SELECT strftime('%d-%m-%Y',date(min(e.date),'unixepoch')) as firstdate, 
-                          strftime('%d-%m-%Y',date(max(e.date),'unixepoch')) as lastdate  
-                  FROM    error_log e LEFT JOIN bots on e.client = bots.ip 
-                  WHERE   domain like ? $option GROUP BY request)";
+    my  $qry  =  "SELECT  count(*) as numrows, 
+                          strftime('%d-%m-%Y',date(min(firstdate),'unixepoch')), 
+                          strftime('%d-%m-%Y',date(max(lastdate),'unixepoch')) 
+                  FROM 
+                  (
+                    SELECT  min(e.date) as firstdate, 
+                            max(e.date) as lastdate  
+                    FROM    error_log e LEFT JOIN bots on e.client = bots.ip 
+                    WHERE   domain like ? $option  GROUP BY request
+                  );";
 
     my $sth = database('sqlserver')->prepare($qry);
     $sth->execute($domain);
